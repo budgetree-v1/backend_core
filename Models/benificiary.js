9806e74e53147e855544eb2c247fa15730d1a12b
SathishBudgetree <sathish@budgetree.in>
2025-05-05 21:19:50 +0530

    X

--- Models/benificiary.js
+++ Models/benificiary.js
@@ -1,56 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const BeneficiarySchema = new Schema(
+const Beneficiary = new Schema(
   {
     User: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
-
-    // Custom Unique Beneficiary ID (e.g., CASH_1746303937763_2252)
-    beneficiary_id: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    // Name
-    beneficiary_name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
-    // Grouped Instrument Details
-    beneficiary_instrument_details: {
-      bank_account_number: { type: String, trim: true },
-      bank_ifsc: { type: String, trim: true },
-      vpa: { type: String, trim: true }, // UPI ID
-    },
-
-    // Grouped Contact Details
-    beneficiary_contact_details: {
-      beneficiary_phone: { type: String, trim: true },
-      beneficiary_country_code: { type: String, trim: true },
-      beneficiary_email: { type: String, trim: true, lowercase: true },
-      beneficiary_address: { type: String, trim: true },
-      beneficiary_city: { type: String, trim: true },
-      beneficiary_state: { type: String, trim: true },
-      beneficiary_postal_code: { type: String, trim: true },
-    },
-
-    // Status Field
-    beneficiary_status: {
-      type: String,
-      enum: ["PENDING", "VERIFIED", "REJECTED"],
-      default: "PENDING",
-    },
+    beneficiary_id: String,
+    beneficiary_name: String,
+    bank_account_number: { type: String, trim: true },
+    bank_ifsc: { type: String, trim: true },
+    vpa: { type: String, trim: true },
+    beneficiary_phone: { type: String, trim: true },
+    beneficiary_email: { type: String, trim: true, lowercase: true },
+    status: Number, //1 success 2 pending 3 failed
   },
   {
     timestamps: true, // Adds createdAt & updatedAt
   }
 );
 
-module.exports = mongoose.model("Beneficiary", BeneficiarySchema);
+module.exports = mongoose.model("Beneficiary", Beneficiary);
--- Services/Helper/pennyDropCron.js
+++ Services/Helper/pennyDropCron.js
@@ -16,3 +16,21 @@ cron.schedule("*/30 * * * * *", async () => {
     console.log(err);
   }
 });
+
+cron.schedule("*/30 * * * * *", async () => {
+  try {
+    console.log("**** Update beneficiay from pennydrop****");
+    let ckbene = await db.Beneficiary.find({ status: 2 });
+    for (let bene of ckbene) {
+      let ck = await db.Transaction.findOne();
+      if (ck.status == 1) {
+        await db.Beneficiary.updateOne({ status: 1 });
+      } else if (ck.status == 3) {
+        await db.Beneficiary.updateOne({ status: 3 });
+      } else {
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
